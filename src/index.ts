e05591a50e02844ce70dc2280a5aa65eedabbdd5
TetieWasTaken <tjvssr@gmail.com>
2025-01-19 19:05:22 +0100

    chore(cli): QoL logging

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,9 @@ program.parse();
 const options = program.opts();
 
 if (options.list) {
-  console.log("\x1b[1m\x1b[34mAvailable Categories:\x1b[0m");
+  console.log(
+    "\x1b[1m\x1b[34mAvailable Categories:\x1b[0m (in increasing size order)",
+  );
   for (const key in dataIndex) {
     console.log(
       `\x1b[1m\x1b[32m${key}:\x1b[0m ${
@@ -25,22 +27,40 @@ if (options.list) {
   process.exit(0);
 }
 
-const fromData = options.from ? getUnitData(options.from) : null;
+if (!options.from && !options.to) {
+  console.error(`\x1b[31mNo "from" or "to" unit specified.\x1b[0m`);
+  console.log(
+    `\x1b[1m\x1b[36mHint:\x1b[0m Use \x1b[33m--list\x1b[0m to list all available units.`,
+  );
+  process.exit(1);
+}
+
+let fromData = options.from ? getUnitData(options.from) : null;
 if (options.from && !fromData) {
   console.error(`\x1b[31mUnit "${options.from}" not found.\x1b[0m`);
   console.log(
     `Did you mean: \x1b[33m${getSuggestions(options.from).join(", ")}\x1b[0m?`,
   );
   process.exit(1);
+} else if (!fromData) {
+  console.warn(
+    `\x1b[33mNo "from" unit specified. Using meter as default unit.\x1b[0m`,
+  );
+  fromData = getUnitData("m");
 }
 
-const toData = options.to ? getUnitData(options.to) : null;
+let toData = options.to ? getUnitData(options.to) : null;
 if (options.to && !toData) {
   console.error(`\x1b[31mUnit "${options.to}" not found.\x1b[0m`);
   console.log(
     `Did you mean: \x1b[33m${getSuggestions(options.to).join(", ")}\x1b[0m?`,
   );
   process.exit(1);
+} else if (!toData) {
+  console.warn(
+    `\x1b[33mNo "to" unit specified. Using meter as default unit.\x1b[0m`,
+  );
+  toData = getUnitData("m");
 }
 
 const value = parseFloat(program.args[0]);
@@ -50,4 +70,8 @@ if (isNaN(value)) {
 }
 
 const result = convert(value, fromData, toData);
-console.log(result);
+console.log(
+  `\x1b[1m${value}\x1b[0m ${fromData?.unit ?? ""} = \x1b[1m${result}\x1b[0m ${
+    toData?.unit ?? ""
+  }`,
+);
